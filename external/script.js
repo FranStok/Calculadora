cd523eabdcdb94c02a1425099a49a8cedf4d26c6
Francico <franciscoastoreca@hotmail.com>
2022-03-07 13:54:16 -0300

    commit

--- external/script.js
+++ external/script.js
@@ -16,8 +16,39 @@ function operacion(op,a,b){
         return suma(a,b);
     if(op=="-")
         return resta(a,b);
-    if(op=="x")
+    if(op=="X")
         return multiplicacion(a,b);
-    if(op=="/")
+    if(op==":")
         return division(a,b);
 }
+let cuenta=null,opVigente=false;
+const display=document.querySelector(".display");
+const numeros=document.querySelectorAll(".numero");
+numeros.forEach(elemento => {
+    elemento.addEventListener("click",(e)=>{
+        cuenta=display.textContent;
+        if(cuenta.length>23) return;
+        if(cuenta==0) return display.textContent=elemento.textContent;
+        return display.textContent=display.textContent + elemento.textContent;
+    })
+});
+const operaciones=document.querySelectorAll(".operacion");
+operaciones.forEach(elemento => {
+    elemento.addEventListener("click",(e)=>{
+        if((opVigente!=false)) return;
+        primerValor=display.textContent;
+        opVigente=true;
+        return display.textContent=primerValor + " " + elemento.textContent + " ";
+    })
+});
+const igual=document.querySelector(".igual");
+igual.addEventListener("click",(e)=>{
+    cuenta=display.textContent;
+    let partes,op;
+    if((/(-|X|:|[+])/).test(cuenta)){
+        op=cuenta.charAt(cuenta.search(/[-X:+]/))
+        partes=cuenta.split(/[-X:+]/);
+        opVigente=false;
+        return display.textContent=operacion(op,parseInt(partes[0].replace(" ","")),parseInt(partes[1].replace(" ","")))
+    }
+})
